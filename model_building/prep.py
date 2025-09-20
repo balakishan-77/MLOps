9cde8b9793f6330b1a066ee52b6c555979f70bf4
Balakishan Gujja <balakishan77@gmail.com>
2025-09-21 00:50:22 +0530

    commit

--- model_building/prep.py
+++ model_building/prep.py
@@ -11,5 +11,5 @@ from huggingface_hub import login, HfApi
 # Define constants for the dataset and output paths
 api = HfApi(token=os.getenv("HF_TOKEN"))
 DATASET_PATH = "hf://datasets/balakishan77/tourism_project/data/tourism.csv"
-bank_dataset = pd.read_csv(DATASET_PATH)
+tourism_dataset = pd.read_csv(DATASET_PATH)
 print("Dataset loaded successfully.")
