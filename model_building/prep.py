59351a1fde49929e2768bc3d2a9f663985200aee
Balakishan Gujja <balakishan77@gmail.com>
2025-09-21 00:03:34 +0530

    first commit

---
+++ model_building/prep.py
@@ -0,0 +1,15 @@
+# for data manipulation
+import pandas as pd
+import sklearn
+# for creating a folder
+import os
+# for data preprocessing and pipeline creation
+from sklearn.model_selection import train_test_split
+# for hugging face space authentication to upload files
+from huggingface_hub import login, HfApi
+
+# Define constants for the dataset and output paths
+api = HfApi(token=os.getenv("HF_TOKEN"))
+DATASET_PATH = "hf://datasets/balakishan77/tourism_project/data/tourism.csv"
+bank_dataset = pd.read_csv(DATASET_PATH)
+print("Dataset loaded successfully.")
